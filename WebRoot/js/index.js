65930b807fac7be36625540af25baa622a1e2ec9
WMQ <WMQ@USER-20170718LO>
2017-11-27 20:05:25 +0800

    test

--- WebRoot/js/index.js
+++
@@ -1,9 +0,0 @@
-function beforeAdd() {
-	document.getElementById("addImg").src = "img/add(2).png";
-	document.getElementById("addInfo").style.color = "#333";
-}
-function afterAdd() {
-	document.getElementById("addImg").src = "img/add(1).png";
-	document.getElementById("addInfo").style.color = "#42A5F5";
-
-}
