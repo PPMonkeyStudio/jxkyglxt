d5a535db02babeff85f906a5c72d534f47ba8102
hy752277380 <33386194+hy752277380@users.noreply.github.com>
2017-11-28 19:39:36 +0800

    topnav

--- WebRoot/js/checkImgChange.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-	$('.checkimg').hover(function() {
-		$(this).attr("src", "http://localhost:8080/teacherms/img/review7.png");
-	}, function() {
-		$(this).attr("src", "http://localhost:8080/teacherms/img/review6.png");
-	});
-});
